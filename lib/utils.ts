6c508d1e3e20d0926794dc8cdb00c8d26cb8c08c
Gabriel Rosa <gabrieldsrosa@gmail.com>
2024-11-13 22:27:01 +0000

    feat: add utils functions to format currency

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { Country } from './types';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatToCurrency(
+  country: Country | string,
+  value: number,
+): string {
+  const countries = {
+    PT: 'pt-PT',
+    US: 'en-US',
+    UK: 'en-UK',
+  };
+
+  const currency = {
+    PT: 'EUR',
+    US: 'USD',
+    UK: 'GBP',
+  };
+
+  // TODO: fix typescript
+  return new Intl.NumberFormat(countries[country], {
+    style: 'currency',
+    currency: currency[country],
+  }).format(value);
 }
